55cb8af569a54e552d7858c69b63029aa7798cf8
John Coote <john_coote@rocketmail.com>
2022-03-03 20:32:05 +1100

    reproduced and then fixed error in view

---
+++ spec/views/animals/show_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe '/animals/show', type: :view do
+  let(:animal) { create(:animal_with_photos) }
+
+  it 'displays animals name' do
+    assign :animal, animal
+
+    render
+
+    expect(rendered).to have_content(animal.name)
+  end
+end
